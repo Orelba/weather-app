5c7d23281abf4bcfabae122f558e44b47de0ff01
Orelba <orel450@gmail.com>
2023-01-13 01:45:41 +0200

    Initial init, initial webpack config

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('success')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHNCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VhdGhlci1hcHAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ3N1Y2Nlc3MnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
